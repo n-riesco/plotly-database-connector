ed7da41827e146ddf619c634c2081ec8e48286b2
Nicolas Riesco <enquiries@nicolasriesco.net>
2017-09-20 12:06:28 +0100

    [WIP] test: do no use co-mocha in test-e2e

--- test/integration_test.js
+++ test/integration_test.js
@@ -1,22 +1,115 @@
-import path from 'path';
 import chromedriver from 'chromedriver';
 import webdriver from 'selenium-webdriver';
-import {expect} from 'chai';
 import electronPath from 'electron';
-import fetch from 'node-fetch';
-import {productName, version} from '../package.json';
+
+import {expect} from 'chai';
+
 import fs from 'fs';
 import fsExtra from 'fs-extra';
+import path from 'path';
+
 import * as settings from '../backend/settings.js';
 import {createStoragePath} from '../backend/utils/homeFiles';
 
 // Utils
 const delay = time => new Promise(resolve => setTimeout(resolve, time));
 
-const TEST_USER = 'plotly-database-connector';
+// Initialize credentials
+clearConnectorFolder();
+saveCredentials();
+console.log("@@@0",
+    settings.getSetting('STORAGE_PATH'),
+    fs.readFileSync(settings.getSetting('SETTINGS_PATH')).toString()
+);
+
+// Start chromedriver
+console.log("@@@1");
+chromedriver.start();
+process.on('exit', chromedriver.stop);
+
+describe('plotly database connector', function() {
+    this.timeout(10 * 60 * 1000);
+
+    before(() => {
+        console.log("@@@2");
+        return delay(3 * 1000).then(() => {
+            console.log("@@@3");
+
+            // Start electron app
+            this.driver = new webdriver.Builder()
+                .usingServer('http://localhost:9515')
+                .withCapabilities({
+                    chromeOptions: {
+                        binary: electronPath,
+                        args: [`app=${path.resolve()}`],
+                    }
+                })
+                .forBrowser('electron')
+                .build();
+
+            return delay(3 * 1000)
+                .then(() => console.log("@@@4"))
+                .then(() => this.driver.getTitle())
+                .then((title) => console.log("@@@5", title))
+                .then(() => this.driver.wait(webdriver.until.titleIs('Plotly Database Connector v2.1.1'), 1 * 1000))
+                .then(() => this.driver.getTitle())
+                .then((title) => console.log("@@@6", title));
+        });
+    });
+
+    after(() => {
+        chromedriver.stop();
+
+        console.log("@@@@@@");
+        if (this.driver) {
+            return delay(1 * 1000)
+                .then(() => this.driver.quit());
+        }
+    });
+
+    it('should create an SSL certificate', () => {
+        console.log("@0");
+
+        let chain = delay(1 * 1000);
+
+        // Fill out SQL Credentials
+        [
+            ['test-input-username', 'masteruser'],
+            ['test-input-host', 'readonly-test-mysql.cwwxgcilxwxw.us-west-2.rds.amazonaws.com'],
+            ['test-input-port', '3306'],
+            ['test-input-password', 'connecttoplotly'],
+            ['test-input-database', 'plotly_datasets']
+        ].forEach(([id, keys]) => {
+            chain = chain
+                .then(() => console.log("@1", id, keys))
+                .then(() => this.driver.findElement(webdriver.By.id(id)).sendKeys(keys));
+        });
+
+        return chain
+            // Connect
+            .then(() => this.driver.findElement(webdriver.By.id('test-connect-button')).click())
+
+            // Wait for certificate to be initialized - could take several minutes
+            .then(() => this.driver.findElement(webdriver.By.className('test-ssl-tab')).click())
+
+            .then(() => new Promise((resolve) => {
+                const waitUntilInitialized = () => {
+                    const isInitialized = this.driver.findElement(webdriver.By.id('test-ssl-initialized'));
+                    console.log("@2", !!isInitialized);
+
+                    if (isInitialized) resolve();
+                    else setTimeout(waitUntilInitialized, 1000);
+                };
+
+                waitUntilInitialized();
+                })
+            );
+    });
+});
+
 function saveCredentials() {
     const usersValue = JSON.stringify([{
-        'username': TEST_USER,
+        'username': 'plotly-database-connector',
         'accessToken': '2MiYq1Oive6RRjC6y9D4u7DjlXSs7k'
     }]);
     if (!fs.existsSync(settings.getSetting('STORAGE_PATH'))) {
@@ -35,77 +128,3 @@ function clearConnectorFolder() {
         console.warn(e);
     }
 }
-
-// Start e2e
-chromedriver.start(); // on port 9515
-process.on('exit', chromedriver.stop);
-
-describe('plotly database connector', function Spec() {
-    this.timeout(1000 * 60 * 10);
-
-    const initialize = async () => {
-        // Open the app
-        await delay(1000); // wait chromedriver start time
-        this.driver = new webdriver.Builder()
-        .usingServer('http://localhost:9515')
-        .withCapabilities({
-            chromeOptions: {
-                binary: electronPath,
-                args: [`app=${path.resolve()}`]
-            }
-        })
-        .forBrowser('electron')
-        .build();
-
-        // Initialize credentials
-        clearConnectorFolder();
-        saveCredentials();
-
-        // class helpers
-        this.findEls = (args) => this.driver.findElements(args);
-        this.findEl = (args) => this.driver.findElement(args);
-        this.byClass = (args) => webdriver.By.className(args);
-        this.byId = (args) => webdriver.By.id(args);
-        this.byCss = (args) => webdriver.By.css(args);
-        this.byPath = (args) => webdriver.By.xpath(args);
-        this.fillInput = (id, value) => (
-            this.findEl(this.byId(id))
-        ).sendKeys(value);
-    }
-
-    before(initialize);
-
-    it('should work', async () => {
-        const title = await this.driver.getTitle();
-
-        await delay(3000);
-
-        // Fill out SQL Credentials
-        [
-            ['test-input-username', 'masteruser'],
-            ['test-input-host', 'readonly-test-mysql.cwwxgcilxwxw.us-west-2.rds.amazonaws.com'],
-            ['test-input-port', '3306'],
-            ['test-input-password', 'connecttoplotly'],
-            ['test-input-database', 'plotly_datasets']
-        ].forEach(setting => {
-            this.fillInput(...setting);
-        });
-
-        // Connect
-        (await this.findEl(this.byId('test-connect-button'))).click();
-
-        (await this.findEl(this.byClass('test-ssl-tab'))).click();        
-
-        // Wait for certificate to be initialized - could take several minutes
-        const isInitialized = () => this.findEl(
-            this.byId('test-ssl-initialized'));
-        let sslIsInitialized = false;
-        while(!sslIsInitialized) {
-            try {
-                sslIsInitialized = Boolean(await isInitialized());
-            } catch(e) {}
-            await delay(1000);
-        }
-
-    });
-});
